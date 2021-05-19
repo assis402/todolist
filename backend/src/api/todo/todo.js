c44146fb6b5aa578c08f2bac131337010410d4d9
Matheus de Assis <assis4002@gmail.com>
2021-05-19 12:58:06 -0300

    Fixing createdAt

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -2,15 +2,15 @@ const { Schema } = require('mongoose')
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-var date = new Date()
-date.setHours(date.getHours() - 3)
+// var date = new Date()
+// date.setHours(date.getHours() - 3)
 
 const todoSchema = new mongoose.Schema({
     description: { type: String, required: true },
     done: { type: Boolean, default: false},
     deadLine: { type: Date },
     ip: { type: String, required: false},
-    createdAt: { type: Date, default: date }
+    createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = restful.model('Todo', todoSchema)
